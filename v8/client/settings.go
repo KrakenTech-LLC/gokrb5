1f51336df96bdff76879a7f70bdbac5a3d8086b8
Evan Hosinski <ehosinski@krakensec.tech>
2025-06-19 14:36:32 -0400

    Add custom dial timeout, proxy dialer, and PAC request options to client settings

--- v8/client/settings.go
+++ v8/client/settings.go
@@ -4,14 +4,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
+
+	"golang.org/x/net/proxy"
 )
 
 // Settings holds optional client settings.
 type Settings struct {
 	disablePAFXFast         bool
 	assumePreAuthentication bool
+	requestPAPac            bool
 	preAuthEType            int32
 	logger                  *log.Logger
+	proxyDialer             proxy.Dialer
+	dialTimout              time.Duration
 }
 
 // jsonSettings is used when marshaling the Settings details to JSON format.
@@ -23,6 +29,7 @@ type jsonSettings struct {
 // NewSettings creates a new client settings struct.
 func NewSettings(settings ...func(*Settings)) *Settings {
 	s := new(Settings)
+	s.dialTimout = time.Second * 5 // default value
 	for _, set := range settings {
 		set(s)
 	}
@@ -90,4 +97,46 @@ func (s *Settings) JSON() (string, error) {
 	}
 	return string(b), nil
 
+}
+
+// SetDialTimout used to configure the client with a custom timeout for establishing network connections
+//
+// s := NewSettings(SetDialTimout(time.Duration))
+func SetDialTimout(d time.Duration) func(*Settings) {
+	return func(s *Settings) {
+		s.dialTimout = d
+	}
+}
+
+// GetDialTimeout returns the client dial timeout duration
+func (s *Settings) GetDialTimeout() time.Duration {
+	return s.dialTimout
+}
+
+// SetProxyDialer used to configure the client to use an upstream proxy for all network communication
+//
+// s := NewSettings(SetProxyDialer(proxy.Dialer))
+func SetProxyDialer(dialer proxy.Dialer) func(*Settings) {
+	return func(s *Settings) {
+		s.proxyDialer = dialer
+	}
+}
+
+// ProxyDialer returns the client proxyDialer instance
+func (s *Settings) ProxyDialer() proxy.Dialer {
+	return s.proxyDialer
+}
+
+// RequestPAPac used to configure the client to request that the KDC include a PAC
+//
+// s := NewSettings(RequestPAPac(true))
+func RequestPAPac(b bool) func(*Settings) {
+	return func(s *Settings) {
+		s.requestPAPac = b
+	}
+}
+
+// RequestPAPac indicates that the client should request that the KDC includes a PAC
+func (s *Settings) RequestPAPac() bool {
+	return s.requestPAPac
 }
